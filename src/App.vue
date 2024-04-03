dab2a4a8dd48ff9258d7da9470a08ed44ab453b8
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-03 11:49:28 +0200

    add state.callApi to mounted in app.vue, tested now works

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,21 @@ export default {
     return {
       state: state,
     }
+  },
+  mounted() {
+    state.callApi() // facciamo la chiamata api al caricamento della pagina per ora
   }
 }
 
-console.log(state.movies)
 
 </script>
 
 <template>
   <h1>ecco il tuo messaggio: {{ state.message }}</h1>
+  <ul>
+    <li v-for="movie in state.movies">{{ movie.original_title }}</li>
+    <!-- momentaneamente inutile, impostiamo struttura per vedere i cambiamenti in pagina -->
+  </ul>
 </template>
 
 <style></style>
--- src/state.js
+++ src/state.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const state = reactive({
     message: "this is a test for boolflix",
     movies: [],
-    api_movie_url: 'https://api.themoviedb.org/3/search/movie?api_key=4c286d2dce5da2b32592ae9e96dd9f32&query=ritorno',
+    api_movie_url: 'https://api.themoviedb.org/3/search/movie?api_key=4c286d2dce5da2b32592ae9e96dd9f32&query=',
     callApi() {
         axios.get(this.api_movie_url).then(response => {
             console.log(response)
