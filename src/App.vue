c4fd5ec3012d360d610062d857a2105cb403f969
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-03 11:21:01 +0200

    add state

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script>
+import { state } from './state.js'
+
+export default {
+  data() {
+    return {
+      state: state,
+    }
+  }
+}
 
 </script>
 
 <template>
-
+  <h1>ecco il tuo messaggio: {{ state.message }}</h1>
 </template>
 
 <style></style>
---
+++ src/state.js
@@ -0,0 +1,7 @@
+import { reactive } from 'vue'
+
+export const state = reactive({
+    message: "this is a test for boolflix",
+    movies: [],
+
+})
