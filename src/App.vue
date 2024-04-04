625e551c91de25be3f6623f4ccc3b69d95b8f91a
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-04 16:13:12 +0200

    add logo, add style

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
 <script>
 import { state } from './state.js'
 import appCard from "./components/appCard.vue";
+import appHeader from "./components/appHeader.vue"
 
 export default {
   components: {
-    appCard
+    appCard,
+    appHeader
   },
   data() {
     return {
       state: state,
     }
   },
-  methods: {
-    search() {
-      this.findFlags()
 
-      state.api_movie_url = `https://api.themoviedb.org/3/search/movie?api_key=4c286d2dce5da2b32592ae9e96dd9f32&language=it-IT&query=${state.searchProducts.split(' ').join('+')}`
-      state.api_tv_series_url = `https://api.themoviedb.org/3/search/movie?api_key=4c286d2dce5da2b32592ae9e96dd9f32&language=it-IT&query=${state.searchProducts.split(' ').join('+')}`
-
-      state.callProductsApi()
-
-      state.searchProducts = ""
-    },
-
-    findFlags() {
-      if (state.products.original_language in state.flags) {
-        //qui dobbiamo mettere che se la key della lingua è compresa nelle flags dobbiamo restituire l'imkmagine invece della stringa
-        return state.flags
-      } else {
-        return state.products.original_language
-      }
-    },
-  },
 
   mounted() {
     //state.callApi() facciamo la chiamata api al caricamento della pagina per ora
@@ -42,12 +24,7 @@ export default {
 </script>
 
 <template>
-  <header>
-    <h1>{{ state.message }}</h1>
-    <input type="search" name="" id="" placeholder="cerca il tuo film o serie tv" v-model="state.searchProducts"
-      @keyup.enter="search()">
-    <button @click="search()">Vai</button>
-  </header>
+  <appHeader />
 
   <main>
     <appCard />
---
+++ src/components/appHeader.vue
@@ -0,0 +1,83 @@
+<script>
+import { state } from '../state';
+
+export default {
+    name: 'appHeader',
+    data() {
+        return {
+            state
+        }
+    },
+    methods: {
+        search() {
+            this.findFlags()
+
+            state.api_movie_url = `https://api.themoviedb.org/3/search/movie?api_key=4c286d2dce5da2b32592ae9e96dd9f32&language=it-IT&query=${state.searchProducts.split(' ').join('+')}`
+            state.api_tv_series_url = `https://api.themoviedb.org/3/search/movie?api_key=4c286d2dce5da2b32592ae9e96dd9f32&language=it-IT&query=${state.searchProducts.split(' ').join('+')}`
+
+            state.callProductsApi()
+
+            state.searchProducts = ""
+        },
+
+        findFlags() {
+            if (state.products.original_language in state.flags) {
+                //qui dobbiamo mettere che se la key della lingua è compresa nelle flags dobbiamo restituire l'imkmagine invece della stringa
+                return state.flags
+            } else {
+                return state.products.original_language
+            }
+        },
+    },
+}
+</script>
+
+<template>
+    <header>
+        <div class="d-flex">
+            <img class="logo"
+                src="C:\Users\Giovanni\Desktop\Boolean\vite-boolflix\public\Beats_Electronics_logo.svg.png" alt="">
+            <h2>{{ state.message }}</h2>
+        </div>
+
+        <div class="inputs d-flex">
+            <input type="search" name="" id="" placeholder="cerca il tuo film o serie tv" v-model="state.searchProducts"
+                @keyup.enter="search()">
+            <button @click="search()">Vai</button>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: black;
+    max-width: 1920px;
+    margin: auto;
+    padding: 0.3rem 2rem;
+
+    .d-flex {
+        display: flex;
+    }
+
+    .inputs {
+        gap: 0.8rem;
+    }
+
+    & h2 {
+        color: rgb(207, 0, 0);
+        font-size: 2rem;
+    }
+
+    & button {
+        padding: 0.3rem 1rem;
+    }
+
+    .logo {
+        width: 100px;
+        margin-right: 0.4rem;
+    }
+}
+</style>
