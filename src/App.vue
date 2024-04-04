d647cfedbe02de841ed3f9f1fe10c88e1338170d
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-04 10:12:51 +0200

    add flags with v-if and v-ekse

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,10 @@ export default {
   },
   methods: {
     search() {
+      this.findFlags()
       const searchSingleMovie = state.searchMovie.split(' ') // con questo dividiamo il testo, mettendo la divisione con spazio non dividiamo in singole lettere
 
-      const searchText = searchSingleMovie.join('+')
+      const searchText = searchSingleMovie.join('+') //riuniamo le parole con un separatore + perchè sia acettato dall'api url
 
       state.api_movie_url = `https://api.themoviedb.org/3/search/movie?api_key=4c286d2dce5da2b32592ae9e96dd9f32&language=it-IT&query=${searchText}`
       state.callApi()
@@ -24,6 +25,9 @@ export default {
     findFlags() {
       if (state.movies.original_language in state.flags) {
         //qui dobbiamo mettere che se la key della lingua è compresa nelle flags dobbiamo restituire l'imkmagine invece della stringa
+        return state.flags
+      } else {
+        return state.movies.original_language
       }
     }
   },
@@ -47,10 +51,14 @@ export default {
   <main>
     <ul>
       <li v-for="movie in state.movies" :key="movie.id">
-        Titolo: {{ movie.title }} /
-        Titolo Originale: {{ movie.original_title }} /
-        Lingua originale: {{ movie.original_language }} /
-        Valutazione: {{ movie.popularity }}
+        <span>Titolo: {{ movie.title }} /</span>
+        <span>Titolo Originale: {{ movie.original_title }} /</span>
+        <!-- <span v-show="movie.original_language in state.flags">Lingua originale: {{ movie.original_language }} /</span> -->
+        <span v-if="movie.original_language in state.flags">
+          <img v-bind:src="state.flags[movie.original_language]" alt="">/
+        </span>
+        <span v-else> Lingua originale: {{ movie.original_language }}/</span>
+        <span>Valutazione: {{ movie.popularity }}</span>
       </li>
       <!-- momentaneamente inutile, impostiamo struttura per vedere i cambiamenti in pagina -->
     </ul>
@@ -62,5 +70,10 @@ export default {
 <style>
 ul {
   list-style: none;
+}
+
+img {
+  width: 50px;
+  height: 30px;
 }
 </style>
--- src/state.js
+++ src/state.js
@@ -3,14 +3,13 @@ import axios from 'axios';
 
 export const state = reactive({
     message: "Welcome to Boolflix",
-    movies: [],
+    movies: '',
     flags: {
-        en: '',
+        en: '../public/flags/1x1/us.svg',
         it: '../public/flags/1x1/it.svg',
-        us: '',
-        fr: '',
-        de: '',
-        jp: '',
+        fr: '../public/flags/1x1/fr.svg',
+        de: '../public/flags/1x1/de.svg',
+        jp: '../public/flags/1x1/jp.svg',
     },
     searchMovie: "",
     api_movie_url: 'https://api.themoviedb.org/3/search/movie?api_key=4c286d2dce5da2b32592ae9e96dd9f32&language=it-IT&query=',
