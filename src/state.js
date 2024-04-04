d20c0a8be2a0463cc2f8811c6644723629558c5a
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-04 09:30:30 +0200

    add flags in public

--- src/state.js
+++ src/state.js
@@ -4,6 +4,14 @@ import axios from 'axios';
 export const state = reactive({
     message: "Welcome to Boolflix",
     movies: [],
+    flags: {
+        en: '',
+        it: '../public/flags/1x1/it.svg',
+        us: '',
+        fr: '',
+        de: '',
+        jp: '',
+    },
     searchMovie: "",
     api_movie_url: 'https://api.themoviedb.org/3/search/movie?api_key=4c286d2dce5da2b32592ae9e96dd9f32&language=it-IT&query=',
     callApi() {
